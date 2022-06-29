264e345764accc4b66a24e9db0f3d6956c66a66f
Enzo Henrique <41205882+egehlen@users.noreply.github.com>
2022-06-28 23:02:35 -0300

    Binary search

--- AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Algorithms/01_LinearSearch.cs
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Algorithms/01_LinearSearch.cs
@@ -1,5 +1,14 @@
 ﻿namespace AlgorithmsAndDataStructures.Algorithms
 {
+    /*
+        Time complexity:
+            - Best: O(1) => when the value matches occurs in the first element
+            - Worst/avg: O(n) => varies according the length of the input
+
+        Space complexity:
+            - Best/worst/avg: O(1) => no allocations done for the algorithm
+    */
+
     public class _01_LinearSearch
     {
         public static int Execute<T>(T[] values, T query)
---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Algorithms/02_BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures.Algorithms
+{
+    public class _02_BinarySearch
+    {
+        public static int Execute(int[] values, int query)
+        {
+            int start = 0;
+            int end = values.Length - 1;
+            int middle = Math.Abs((start + end) / 2);
+
+            while (!values[middle].Equals(query) && start <= end)
+            {
+                if (query < values[middle])
+                    end = middle - 1;
+                else
+                    start = middle + 1;
+
+                middle = Math.Abs((start + end) / 2);
+            }
+
+            return values[middle] == query ? middle : -1;
+        }
+    }
+}
